1fc6d1fc90578ca9940b68a08da97653aa47892b
Owen Ekhator <owenekhator@rocketmail.com>
2019-03-02 01:27:52 -0600

    added some logic and imported styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
-import { Container, SearchDiv, ResultDiv } from "./HomeStyle";
+import { Container, SearchDiv, ResultWrapper,ResultDiv } from "./HomeStyle";
 import { Input } from "antd";
 
 const Search = Input.Search;
 
 const Home = () => {
+  const [value, setValue] = useState("");
+  function handleSubmit(value) {
+    if (!value) {
+      setValue("Error: Please provide a search query first");
+    } else {
+      setValue("");
+    }
+  }
+
   return (
     <Container>
       <SearchDiv>
-      <h2>BOOK FINDER</h2>
+        <h2>BOOK FINDER</h2>
         <Search
           placeholder="input book title..."
-          onSearch={value => console.log(value)}
+          onSearch={value => handleSubmit(value)}
           size="large"
           style={{ width: "60%" }}
           enterButton="Search"
         />
       </SearchDiv>
 
-      <ResultDiv />
+      <ResultWrapper>
+        {<h3>{value}</h3> &&
+        <ResultDiv></ResultDiv>}
+      </ResultWrapper>
     </Container>
   );
 };
