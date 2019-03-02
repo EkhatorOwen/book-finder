515a7943335c3176ff8568ab62baa196a4400b49
Owen Ekhator <owenekhator@rocketmail.com>
2019-03-01 21:09:57 -0600

    added homestyle

---
+++ src/components/HomeStyle.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+width: 100vw;
+height: 100vh;
+`
+
+export const SearchDiv = styled.div`
+height: 40%;
+border: 1px solid red;
+`
+
+export const ResultDiv = styled.div`
+height: 60% ;
+border: 1px solid green;
+`
