8489597d853d3ed12498aefd6cde3c7ab1d2edbd
Owen Ekhator <owenekhator@rocketmail.com>
2019-03-03 01:48:14 -0600

    modified home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,38 @@
 import React, { useState } from "react";
-import { Container, SearchDiv, ResultWrapper,ResultDiv } from "./HomeStyle";
-import { Input } from "antd";
+import {
+  Container,
+  SearchDiv,
+  ResultWrapper,
+  ResultDiv,
+  Link
+} from "./HomeStyle";
+import { Input, Card } from "antd";
 
 const Search = Input.Search;
+const { Meta } = Card;
 
 const Home = () => {
-  const [value, setValue] = useState("");
-  function handleSubmit(value) {
+  const [value, setValue] = useState(
+    "Error: Please provide a search query first"
+  );
+  const [isEmpty, updateEmpty] = useState(false);
+  const [books, updateBooks] = useState([]);
+
+  async function handleSubmit(value) {
+    // console.log(process.env.REACT_APP_GOOGLE_API_KEY)
     if (!value) {
-      setValue("Error: Please provide a search query first");
+      updateEmpty(true);
+      updateBooks([]);
     } else {
-      setValue("");
+      updateEmpty(false);
+      const res = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${value}&key=${
+          process.env.REACT_APP_GOOGLE_API_KEY
+        }`
+      );
+      const { items } = await res.json();
+      console.log(items);
+      updateBooks(items);
     }
   }
 
@@ -19,7 +41,7 @@ const Home = () => {
       <SearchDiv>
         <h2>BOOK FINDER</h2>
         <Search
-          placeholder="input book title..."
+          placeholder="input book title or author..."
           onSearch={value => handleSubmit(value)}
           size="large"
           style={{ width: "60%" }}
@@ -28,8 +50,33 @@ const Home = () => {
       </SearchDiv>
 
       <ResultWrapper>
-        {<h3>{value}</h3> &&
-        <ResultDiv></ResultDiv>}
+        {isEmpty && <h3>{value}</h3>}
+        <ResultDiv>
+          {books.map((book, i) => (
+            <Card
+              key={i}
+              hoverable
+              style={{ width: 240, padding: "5px" }}
+              cover={
+                <img
+                  alt={book.volumeInfo.title}
+                  src={book.volumeInfo.imageLinks.thumbnail}
+                />
+              }
+            >
+              <Meta
+                title={`${book.volumeInfo.title}`}
+                description={`Author: ${book.volumeInfo.authors.map(
+                  elem => elem
+                )}`}
+              />
+              <p>Published by: {book.volumeInfo.publisher}</p>
+              <Link target="_blank" href={book.volumeInfo.infoLink}>
+                Click
+              </Link>
+            </Card>
+          ))}
+        </ResultDiv>
       </ResultWrapper>
     </Container>
   );
