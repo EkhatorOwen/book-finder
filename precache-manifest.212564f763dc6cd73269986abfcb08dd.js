41fc9ac881ec3a18445423daaa7c81a64fe77017
Owen Ekhator <owenekhator@rocketmail.com>
2019-03-03 02:04:00 -0600

    Updates

---
+++ precache-manifest.212564f763dc6cd73269986abfcb08dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "736fd4229ce5946055cf",
+    "url": "/book-finder/static/js/runtime~main.736fd422.js"
+  },
+  {
+    "revision": "941b1fbf20192b488fa9",
+    "url": "/book-finder/static/js/main.941b1fbf.chunk.js"
+  },
+  {
+    "revision": "5b0e3bbb71fb49057a4f",
+    "url": "/book-finder/static/js/2.5b0e3bbb.chunk.js"
+  },
+  {
+    "revision": "941b1fbf20192b488fa9",
+    "url": "/book-finder/static/css/main.fb28cfb4.chunk.css"
+  },
+  {
+    "revision": "5b0e3bbb71fb49057a4f",
+    "url": "/book-finder/static/css/2.179ab14f.chunk.css"
+  },
+  {
+    "revision": "aa8b10e7cd2e87a48caaf26fc5993911",
+    "url": "/book-finder/index.html"
+  }
+];
