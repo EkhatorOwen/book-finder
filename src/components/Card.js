0c45c404fca7d8852ec1022d50c26f457a06358f
Owen Ekhator <owenekhator@rocketmail.com>
2019-03-09 20:38:14 -0600

    styling card component

---
+++ src/components/Card.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { CardContainer, ImageContainer, TextContainer, Text } from "./CardStyle";
+import { Image } from "./HomeStyle";
+
+const Card = ({ title, author, publisher, imageLink, bookLink }) => {
+  return (
+    <>
+      <CardContainer>
+        <ImageContainer>
+        <Image
+          height="50"
+          width="50"
+        />
+        </ImageContainer>
+        <TextContainer> 
+        <Text><b>{title}</b></Text>
+        
+        </TextContainer>
+      </CardContainer>
+    </>
+  );
+};
+
+export default Card;
+
+//title
+//author
+//published by
+//link to google book
+//link to image
--- src/components/CardStyle.js
+++ src/components/CardStyle.js
@@ -1,1 +1,22 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+  border: 4px solid blue;
+  height: 100px;
+  width: 150px;
+
+`;
+
+export const ImageContainer = styled.div`
+  border: 4px solid yellow;
+  height: 50%;
+  width: 50%;
+`;
+
+export const TextContainer = styled.div`
+    border: 4px solid green;
+    height: 50%;
+    width: 50%;
+`;
+
+export const Text = styled.p``
